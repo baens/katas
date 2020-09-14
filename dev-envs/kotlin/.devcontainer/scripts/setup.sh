56c6fe0228bba8e9c59538cafe8473f2f19c6726
Erik Lindblom <erik@baens.net>
2020-09-14 06:09:18 -0600

    Testing out going with debian, not sure if this is the right thing todo or not

--- dev-envs/kotlin/.devcontainer/scripts/setup.sh
+++ dev-envs/kotlin/.devcontainer/scripts/setup.sh
@@ -1,34 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-USERNAME="vscode"
-USER_UID=1000
-USER_GID=1000
+function setupPackages() {
+    apt-get update
+    apt-get install -y git curl unzip
+}
 
-cat >> /etc/skel/.bashrc <<'EOF'
-export SDKMAN_DIR="/usr/local/sdkman"
-source "/usr/local/sdkman/bin/sdkman-init.sh"
+function setupAsdk() {
+    mkdir -p /etc/skel
+    cat >> /etc/skel/.bashrc <<-'EOF'
+    if [[ ! -d "$HOME/.asdf" ]]; then
+        git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+    fi
+
+    . $HOME/.asdf/asdf.sh
+    asdf plugin-add gradle https://github.com/rfrancis/asdf-gradle.git
+    asdf plugin-add java https://github.com/halcyon/asdf-java.git
+
+    . ~/.asdf/plugins/java/set-java-home.bash
+
+    asdf install
 EOF
+}
 
-if id -u $USERNAME > /dev/null 2>&1; then
-    # User exists, update if needed
-    if [ "$USER_GID" != "$(id -G $USERNAME)" ]; then
-        groupmod --gid $USER_GID $USERNAME
-        usermod --gid $USER_GID $USERNAME
+function setupUser() {
+    local -r USERNAME="vscode"
+    local -r USER_UID=1000
+    local -r USER_GID=1000
+
+    if id -u $USERNAME > /dev/null 2>&1; then
+        # User exists, update if needed
+        if [ "$USER_GID" != "$(id -G $USERNAME)" ]; then
+            groupmod --gid $USER_GID $USERNAME
+            usermod --gid $USER_GID $USERNAME
+        fi
+        if [ "$USER_UID" != "$(id -u $USERNAME)" ]; then
+            usermod --uid $USER_UID $USERNAME
+        fi
+    else
+        # Create user
+        groupadd --gid $USER_GID $USERNAME
+        useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME
     fi
-    if [ "$USER_UID" != "$(id -u $USERNAME)" ]; then
-        usermod --uid $USER_UID $USERNAME
-    fi
-else
-    # Create user
-    groupadd --gid $USER_GID $USERNAME
-    useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME
-fi
 
+    echo "vscode ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+}
 
-apt update
-apt install zip unzip sudo
-export SDKMAN_DIR="/usr/local/sdkman" && curl -s "https://get.sdkman.io" | bash
-
-source "/usr/local/sdkman/bin/sdkman-init.sh"
-
-sdk install gradle
+setupPackages
+setupAsdk
+setupUser
---
+++ dev-envs/kotlin/README.md
@@ -0,0 +1,6 @@
+A simple example of a workspace, with kotlin, gradle, and a Database.
+
+TODOs:
+* Get kotlin language server working
+* Example of a Kotlin test connecting to database
+* Web server
